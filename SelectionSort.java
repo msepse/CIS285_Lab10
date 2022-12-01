533bff7302950e7e476dc3d42dd6a9eec50a45aa
Michael Sepsey <msepsey@umich.edu>
2022-12-01 17:45:13 -0500

    master fixed test case errors by modifying SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,12 +1,12 @@
-public class SelectionSort 
+public class SelectionSort
 {
 	private int temp;
 	/** Creates a new instance of SelectionSort */
-	public SelectionSort() 
+	public SelectionSort()
 	{
-		
+
 	}
-	
+
 	/* A simple SelectionSort algorithm
 	 * pre-condition: items are completely unsorted
 	 * post-condition: items are completely sorted
@@ -14,21 +14,21 @@ public class SelectionSort
 	 * outputs: int array of size n (sorted)
 	 * special conditions: none
 	 */
-	public int[] basicSelectionSort(int[] x) 
+	public int[] basicSelectionSort(int[] x)
 	{
-		for (int i = 0; i <= x.length-1; ++i) 
+		for (int i = 0; i <= x.length-1; ++i)
 		{
-			for (int j= i+1; j <= x.length-1; ++j) 
+			for (int j= i+1; j <= x.length-1; ++j)
 			{
-				if (x[i] > x[j]) 
+				if (x[i] > x[j])
 				{
 					temp = x[i];
 					x[i] = x[j];
-					temp = x[j];
+					x[j] = temp;
 				}
 			} // end of inner for loop
 		} // end of outer for loop
-		
+
 		return x;
 	} // end of basicSelectionSort method
 }
