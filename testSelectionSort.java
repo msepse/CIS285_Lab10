f51bb45a048d44655aa032a64177db0b591bd34d
Michael Sepsey <msepsey@umich.edu>
2022-12-01 17:32:39 -0500

    Master added testSelectionSort.java to repository

---
+++ testSelectionSort.java
@@ -0,0 +1,39 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class testSelectionSort
+{
+    @Test
+    public void test()
+    {
+        testPositive();
+        testNegative();
+        testMixed();
+        testDuplicates();
+    }
+
+    @Test
+    public void testPositive()
+    {
+        
+    }
+
+    @Test
+    public void testNegative()
+    {
+        
+    }
+
+    @Test
+    public void testMixed()
+    {
+        
+    }
+
+    @Test
+    public void testDuplicates()
+    {
+        
+    }
+}
